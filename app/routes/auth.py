66a83ff27be4eab40cbbaced36b00aebe9ba85b1
subhash varma <subhashvarma220@gmail.com>
2025-04-02 11:43:04 +0530

    sd;

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.utils.auth import create_access_token, create_refresh_token, verify_password,verify_token, hash_password
 from app.utils.database import get_db
 from app.models.usermodel import User
-
+from fastapi.responses import JSONResponse
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
@@ -13,13 +13,16 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 @router.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == form_data.username).first()
+    
     if not user or not verify_password(form_data.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"detail": "Invalid credentials"})
+    
     if not user.is_verified:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is not verified") 
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"detail": "User is not verified"})
 
     access_token = create_access_token({"sub": user.email})
     refresh_token = create_refresh_token({"sub": user.email})
+
     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
 
 @router.get("/validate")
